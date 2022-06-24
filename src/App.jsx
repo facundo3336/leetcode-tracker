fb92baa2325e3b8b02c125137c1ea9cb6461fb6e
facundo3336 <fcv3336@gmail.com>
2022-06-24 15:47:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-import { useState } from "react";
-import { Checkbox } from "./components/checkbox/checkbox";
-
 export const App = () => {
-  const [checkbox, setCheckbox] = useState(false);
-
-  function handleChange(value) {
-    setCheckbox(value);
-  }
-
-  return <Checkbox checked={checkbox} onChange={handleChange} label="Holas?" />;
+  return <></>;
 };
