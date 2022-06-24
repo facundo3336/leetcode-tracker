241cd2d78c47f8395df63ca3302f934f03ca79c3
facundo3336 <fcv3336@gmail.com>
2022-06-24 14:34:38 -0300

    Create input component

---
+++ src/components/input/input.jsx
@@ -0,0 +1,37 @@
+import "./input.scss";
+
+export const Input = ({
+  value,
+  onChange,
+  label,
+  icon,
+  placeholder = "",
+  disable,
+  error,
+}) => {
+  function handleChange(e) {
+    onChange(e.target.value);
+  }
+
+  return (
+    <div className={`input`}>
+      {label !== undefined && <label>{label}</label>}
+      <div
+        className={`inputContainer ${disable !== undefined && "disable"} ${
+          error && "inputError"
+        }`}
+      >
+        <div className="inputIconContainer">{icon && icon}</div>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => handleChange(e)}
+          placeholder={placeholder !== undefined && placeholder}
+          disabled={disable}
+        />
+        {error && <i className="fa-solid fa-circle-exclamation"></i>}
+      </div>
+      {error && <span>{error}</span>}
+    </div>
+  );
+};
