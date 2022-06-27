5bd4048ffd5a5f95d07e06961f24f5e519ef14ef
facundo3336 <fcv3336@gmail.com>
2022-06-27 12:27:19 -0300

    Create sign up page

--- src/pages/signup/signup.jsx
+++ src/pages/signup/signup.jsx
@@ -1,16 +1,22 @@
+import "../../styles/login-signup.scss";
 import { useState } from "react";
 import { Input } from "../../components/input/input";
 import Key from "@mui/icons-material/Key";
 import MailOutline from "@mui/icons-material/MailOutline";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import { Checkbox } from "../../components/checkbox/checkbox";
 import { Button } from "../../components/button/button";
 import { Link } from "react-router-dom";
-import "../../styles/login-signup.scss";
 
-export const Login = () => {
+export const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   const [checkbox, setCheckbox] = useState(false);
+
+  function handleChangeName(value) {
+    setName(value);
+  }
 
   function handleChangeEmail(value) {
     setEmail(value);
@@ -28,11 +34,18 @@ export const Login = () => {
     <div className="loginSignupPage">
       <div className="loginSignupInformationContainer">
         <div className="loginSignupLogo"></div>
-        <h4>Welcome back!</h4>
-        <span className="loginSignupPageSubtitle">
-          Login to access your Stack Account
+        <h4>Sign up!</h4>
+        <span className="loginSignupPageSubtitle onlySignUpsubtitleWeight">
+          Create an account with FlowDash
         </span>
         <div className="loginSignupInputsContainer">
+          <Input
+            value={name}
+            onChange={handleChangeName}
+            icon={<AccountBoxIcon />}
+            label="Name:"
+            placeholder="Jhon Doe"
+          />
           <Input
             value={email}
             onChange={handleChangeEmail}
@@ -50,17 +63,17 @@ export const Login = () => {
           <Checkbox
             onChange={handleCheckbox}
             checked={checkbox}
-            label="Remember me"
+            label="I accept"
+            extra={<Link to="/">Terms and Conditions</Link>}
           />
         </div>
         <Button
-          className="loginSignupButton"
+          className="loginSignupButton onlySignupButtonMargin"
           color="primary"
-          children="Login"
+          children="Create account"
         />
-        <Button color="link" children="Forgot password?" />
         <span className="loginSignupParagraph">
-          Do you have an account? <Link to="/">Sign up!</Link>
+          <Link to="/auth/signin">Have an account?</Link>
         </span>
       </div>
       <div className="loginSignupImage"></div>
